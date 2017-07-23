8c3d34654643d67fa959f4767d5916a860ce7046
Faisal Zaman <faisalzamnn@gmail.com>
2017-07-23 23:40:52 +0800

    added sound and flashlight

--- app/src/main/java/com/jquery404/flashlight/main/MainActivity.java
+++ app/src/main/java/com/jquery404/flashlight/main/MainActivity.java
@@ -1,43 +1,63 @@
 package com.jquery404.flashlight.main;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.audiofx.Visualizer;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
-import android.widget.TextView;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.os.Message;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.util.List;
+
 import com.jquery404.flashlight.R;
-import com.pddstudio.easyflashlight.EasyFlashlight;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 
 /**
  * Created by Faisal on 6/30/17.
  */
 
-public class MainActivity extends BaseCompatActivity {
+public class MainActivity extends BaseCompatActivity implements SurfaceHolder.Callback {
 
     @BindView(R.id.myvisualizerview)
     VisualizerView visualizerView;
 
-    @BindView(R.id.latlngHeading)
-    TextView tvHeading;
+    @BindView(R.id.bit_a)
+    View bitA;
+
 
     private MediaPlayer mMediaPlayer;
     private Visualizer mVisualizer;
-    public float intensity = 0;
-    private boolean toggleTorch;
-
+    public float[] intensity = new float[4];
+    private Camera camera;
+    private boolean isFlashOn;
+    private boolean hasFlash;
+    Camera.Parameters params;
+    SurfaceHolder mHolder;
+    SurfaceView preview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,25 +65,75 @@ public class MainActivity extends BaseCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+        checkFlash();
+        isFlashOn = false;
 
-        boolean isFlashAvailable = getApplicationContext().getPackageManager().
-                hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+        getCamera();
+        initAudio();
+    }
 
-        if (!isFlashAvailable) {
-            showMessage();
+
+    public void checkFlash() {
+        hasFlash = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        if (!hasFlash) {
+            android.app.AlertDialog.Builder alert = new android.app.AlertDialog.Builder(MainActivity.this);
+            alert.setTitle("Error!");
+            alert.setMessage("Your phone does not have the flash!");
+            alert.setPositiveButton("OK", (d, i) -> finish());
         } else {
-            initTorch();
-            initAudio();
+            Toast.makeText(this, "Have", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        mHolder = holder;
+        try {
+            camera.setPreviewDisplay(mHolder);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    private void initTorch() {
-        EasyFlashlight.init(this);
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        camera.stopPreview();
+        mHolder = null;
     }
+
+
+    // getting camera parameters
+    private void getCamera() {
+        preview = (SurfaceView) findViewById(R.id.PREVIEW);
+        mHolder = preview.getHolder();
+        //mHolder.addCallback(this);
+        mHolder.addCallback(MainActivity.this);
+        //Android < 2.3.6 ha bisogno di sto hack
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        if (camera == null) {
+            try {
+
+
+                camera = Camera.open();
+                params = camera.getParameters();
+                try {
+                    camera.setPreviewDisplay(mHolder);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } catch (RuntimeException e) {
+                Log.e("SUCA.", e.getMessage());
+            }
+        }
+    }
+
 
     private void initAudio() {
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
-        mMediaPlayer = MediaPlayer.create(this, R.raw.attention);
+        mMediaPlayer = MediaPlayer.create(this, R.raw.till_i_co);
 
         setupVisualizerFxAndUI();
         mVisualizer.setEnabled(true);
@@ -83,14 +153,19 @@ public class MainActivity extends BaseCompatActivity {
             public void onWaveFormDataCapture(Visualizer visualizer, byte[] waveform, int samplingRate) {
                 visualizerView.updateVisualizer(waveform);
                 if (waveform != null) {
-                    intensity = ((float) waveform[0] + 128f) / 256;
+                    intensity[0] = ((float) waveform[0] + 128f) / 256;
+                    intensity[1] = ((float) waveform[1] + 128f) / 256;
+                    intensity[2] = ((float) waveform[2] + 128f) / 256;
+                    intensity[3] = ((float) waveform[3] + 128f) / 256;
 
-                    if (intensity < 0.5f) {
-
+                    if (intensity[3] < 0.5f) {
+                        bitA.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.bit6));
+                        turnOnFlash();
                     } else {
+                        bitA.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.bit7));
+                        turnOffFlash();
                     }
 
-                    Log.d("vis", String.valueOf(intensity));
                 }
             }
 
@@ -105,8 +180,8 @@ public class MainActivity extends BaseCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        getCamera();
     }
-
 
     @Override
     protected void onResume() {
@@ -116,6 +191,9 @@ public class MainActivity extends BaseCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+
+        // on pause turn off the flash
+        turnOffFlash();
 
         if (isFinishing() && mMediaPlayer != null) {
             mVisualizer.release();
@@ -127,7 +205,11 @@ public class MainActivity extends BaseCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-
+        // on stop release the camera
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
     }
 
     public static void start(Context context) {
@@ -136,31 +218,50 @@ public class MainActivity extends BaseCompatActivity {
         context.startActivity(intent);
     }
 
-    private void showMessage() {
-        AlertDialog alert = new AlertDialog.Builder(this).create();
-        alert.setTitle("Error!!");
-        alert.setMessage("Your device doesn't support flash light!");
-        alert.setButton(DialogInterface.BUTTON_POSITIVE, "Ok", (d, w) -> {
-            finish();
-            System.exit(0);
-        });
-    }
 
-    @OnClick(R.id.btn_facebook)
-    public void onToggleClick() {
-        toggleTorch = !toggleTorch;
-
-        if (toggleTorch) {
-            Toast.makeText(this, "permission", Toast.LENGTH_SHORT).show();
-            EasyFlashlight.getInstance().turnOn();
+    @OnClick(R.id.btn_play)
+    public void onTogglePlay() {
+        if (mMediaPlayer.isPlaying()) {
+            mMediaPlayer.pause();
         } else {
-            EasyFlashlight.getInstance().turnOff();
+            mMediaPlayer.start();
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        EasyFlashlight.getInstance().onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    /*
+    * Turning On flash
+    */
+    private void turnOnFlash() {
+        if (!isFlashOn) {
+            if (camera == null || params == null) {
+                return;
+            }
+
+            isFlashOn = true;
+
+            params = camera.getParameters();
+            params.setFlashMode(Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(params);
+            camera.startPreview();
+        }
     }
+
+    private void turnOffFlash() {
+        if (isFlashOn) {
+            if (camera == null || params == null) {
+                return;
+            }
+
+            isFlashOn = false;
+
+            params = camera.getParameters();
+            params.setFlashMode(Parameters.FLASH_MODE_OFF);
+            camera.setParameters(params);
+            camera.stopPreview();
+        }
+    }
+
+
 
 }
--- app/src/main/java/com/jquery404/flashlight/main/VisualizerView.java
+++ app/src/main/java/com/jquery404/flashlight/main/VisualizerView.java
@@ -55,13 +55,20 @@ public class VisualizerView extends View {
     }
 
     public double computedbAmp(byte[] audioData) {
-        double amplitude = 0;
-        for (int i = 0; i < audioData.length / 2; i++) {
+        double sum=0;
+        for (int i = 0; i < audioData.length/2; i++) {
+            double y = (audioData[i*2] | audioData[i*2+1] << 8) / 32768.0;
+            sum += y * y;
+        }
+        double rms = Math.sqrt(sum / audioData.length/2);
+        return  20.0*Math.log10(rms);
+
+        /*for (int i = 0; i < audioData.length / 2; i++) {
             double y = (audioData[i * 2] | audioData[i * 2 + 1] << 8) / 32768.0;
             amplitude += y * y;
         }
         double rms = Math.sqrt(amplitude / audioData.length / 2);
-        return 20.0 * Math.log10(rms);
+        return 20.0 * Math.log10(rms);*/
     }
 
     @Override
@@ -75,15 +82,26 @@ public class VisualizerView extends View {
 
         mRect.set(0, 0, getWidth(), getHeight());
 
-        for (int i = 0; i < mBytes.length - 1; i++) {
+        /*for (int i = 0; i < mBytes.length - 1; i++) {
             mPoints[i * 4] = mRect.width() * i / (mBytes.length - 1);
             mPoints[i * 4 + 1] = mRect.height() / 2 + ((byte) (mBytes[i] + 128)) * (mRect.height() / 2) / 128;
             mPoints[i * 4 + 2] = mRect.width() * (i + 1) / (mBytes.length - 1);
             mPoints[i * 4 + 3] = mRect.height() / 2 + ((byte) (mBytes[i + 1] + 128)) * (mRect.height() / 2) / 128;
+        }*/
+
+        // bar
+        for (int i = 0; i < mBytes.length / 2; i++) {
+            mPoints[i * 4] = i * 8;
+            mPoints[i * 4 + 1] = 0;
+            mPoints[i * 4 + 2] = i * 8;
+            byte rfk = mBytes[2 * i];
+            byte ifk = mBytes[2 * i + 1];
+            float magnitude = (float) (rfk * rfk + ifk * ifk);
+            int dbValue = (int) (10 * Math.log10(magnitude));
+            mPoints[i * 4 + 3] = (float) (dbValue * 7);
         }
 
         canvas.drawLines(mPoints, mForePaint);
     }
-
 
 }
